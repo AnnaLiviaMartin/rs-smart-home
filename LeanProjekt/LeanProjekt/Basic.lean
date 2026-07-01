cdaccc1b0147cb44d58b96eca5b73633040e0105
Anna-Livia Martin <119300861+AnnaLiviaMartin@users.noreply.github.com>
2026-07-01 11:58:49 +0200

    feat: add lean code

--- LeanProjekt/LeanProjekt/Basic.lean
+++ LeanProjekt/LeanProjekt/Basic.lean
@@ -1,23 +1,19 @@
-import Mathlib.Tactic
--- Bitte inhalt löschen und mit eigenen code füllen!!!
+/-
+  Smart Home – Allergröbstes Modell
 
+  Modelliert werden nur:
+  - Räume
+  - Nachbarschaft
+  - Symmetrie
+  - Keine Selbstnachbarschaft
+  - alle Räume liegen in einem Gebäude
+-/
+import Mathlib
 
--- axiom axm0_1 : d in N
-axiom d : Int -- d ist eine Ganze Zahl
-axiom axm0_2 : d > 0
+structure Raum where
+  my_id: Nat
+  nachbar_ids: Finset Nat
 
-#check axm0_2 -- aufrufen, falls ich vergessen habe, was in dem axiom stand
-#check 4
+axiom keineSelbstNachbarschaft : ∀ r : Raum, ¬ (r.my_id ∈ r.nachbar_ids)
 
--- @grind sagt aus, dass das von grind ausgepackt werden darf? Wird in grind DB geschrieben??
-@[grind]def inv0_1 (n : Int) := n >= 0 -- backslash gt für größer glcih
-def inv0_2 (n : Int) : Prop := n <= d -- man definiert eine ivariante als funktion, die sagt, ob Bedingung erfüllt ist
-
-#check inv0_1
-
--- ist ein prädikat
-def init (n' : Int) := n' = 0
-
-def init_post (n' : Int) := n' = 0 -- int ist nur ein anderes symbol für ℤ
-
-def MLout_pre (n : Int) := n < d --KFZ fährt von Inselbrücke runter
+axiom symmetrie : ∀ r₁ r₂ : Raum, r₁.my_id ∈ r₂.nachbar_ids ∧ r₂.my_id ∈ r₁.nachbar_ids
